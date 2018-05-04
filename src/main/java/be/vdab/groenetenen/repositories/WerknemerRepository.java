995c0a29bbcc0902968847cc53358cd89ff6367e
Liselotte <liselotte.des@gmail.com>
2018-05-04 14:58:36 +0200

    3.2. (werknemers met filiaal zonder entity graph)

---
+++ src/main/java/be/vdab/groenetenen/repositories/WerknemerRepository.java
@@ -0,0 +1,7 @@
+package be.vdab.groenetenen.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import be.vdab.groenetenen.entities.Werknemer;
+
+public interface WerknemerRepository extends JpaRepository<Werknemer, Long> {}
---
+++ src/main/java/be/vdab/groenetenen/web/WerknemerController.java
@@ -0,0 +1,26 @@
+package be.vdab.groenetenen.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.groenetenen.services.WerknemerService;
+
+@Controller
+@RequestMapping("werknemers")
+class WerknemerController {
+	
+	private final WerknemerService werknemerService;
+	
+	WerknemerController(WerknemerService werknemerService) {
+		this.werknemerService = werknemerService;
+	}
+	
+	private static final String WERKNEMERS_VIEW = "werknemers/werknemers";
+	
+	@GetMapping
+	ModelAndView lijst() {
+		return new ModelAndView(WERKNEMERS_VIEW, "werknemers", werknemerService.findAll());
+	}
+}
