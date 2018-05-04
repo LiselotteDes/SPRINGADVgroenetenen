b1e47eb50b02d2dc00910ddf740815ed5b84a1a5
Liselotte <liselotte.des@gmail.com>
2018-05-04 14:04:44 +0200

    3.1.1. Automatische String naar Entity converter

--- src/main/java/be/vdab/groenetenen/web/FiliaalController.java
+++ src/main/java/be/vdab/groenetenen/web/FiliaalController.java
@@ -1,13 +1,18 @@
 package be.vdab.groenetenen.web;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import be.vdab.groenetenen.entities.Filiaal;
 import be.vdab.groenetenen.services.FiliaalService;
 
 @Controller
@@ -31,5 +36,25 @@ class FiliaalController {
 			return new ModelAndView(VAN_TOT_POSTCODE_VIEW);
 		}
 		return new ModelAndView(VAN_TOT_POSTCODE_VIEW).addObject("filialen", filiaalService.findByPostcode(form.getVan(), form.getTot()));
+	}
+	
+	// "Automatische String naar Entity converters"
+	private static final String FILIAAL_VIEW = "filialen/filiaal";
+	private static final String REDIRECT_FILIAAL_NIET_GEVONDEN = "redirect:/";
+	/*
+	 * Je geeft aan dat de method read requests verwerkt met een URL vermeld bij @RequestMapping, 
+	 * gevolgd door een path variabele die je hier aanduidt met de naam id.
+	 */
+	@GetMapping("{id}")
+	ModelAndView read(@PathVariable("id") Optional<Filiaal> filiaal, RedirectAttributes redirectAttributes) {
+		/*
+		 * Je geeft hierboven aan dat Spring de parameter filiaal moet invullen met de inhoud van de path variabele id.
+		 * Spring gebruikt op dit moment de converter van Spring data die de path variabele omzet in een Optional<Filiaal> object.
+		 */
+		if (filiaal.isPresent()) {
+			return new ModelAndView(FILIAAL_VIEW).addObject(filiaal.get());
+		}
+		redirectAttributes.addAttribute("fout", "Filiaal niet gevonden");
+		return new ModelAndView(REDIRECT_FILIAAL_NIET_GEVONDEN);
 	}
 }
