28ed6331d6cfaef9c961cf19b09da1426967f211
Liselotte <liselotte.des@gmail.com>
2018-05-09 13:45:34 +0200

    7.1.3. Taal en land van gebruiker: SessionLocaleResolver

--- src/main/java/be/vdab/groenetenen/web/WebConfiguratie.java
+++ src/main/java/be/vdab/groenetenen/web/WebConfiguratie.java
@@ -4,7 +4,8 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+//import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration		/*Je tikt @Configuration voor een class waarbinnen je met @Bean Spring beans maakt.*/
 class WebConfiguratie {
@@ -16,8 +17,10 @@ class WebConfiguratie {
 	 * Spring houdt die bean bij zolang de website draait.
 	 */
 	@Bean
-	FixedLocaleResolver localeResolver() {
-		return new FixedLocaleResolver(new Locale("fr", "BE"));		/*De class Locale stelt een combinatie van een land en een taal voor.*/
+	/*FixedLocaleResolver*/ 
+	SessionLocaleResolver localeResolver() {
+		/*return new FixedLocaleResolver(new Locale("fr", "BE"));		/*De class Locale stelt een combinatie van een land en een taal voor.*/
+		return new SessionLocaleResolver();
 	}
 	
 }
