9263cd083dea246ca319927772512077d6880ede
Liselotte <liselotte.des@gmail.com>
2018-05-09 14:10:28 +0200

    7.1.4. Taal en land van gebruiker: LocaleChangeInterceptor

--- src/main/java/be/vdab/groenetenen/web/WebConfiguratie.java
+++ src/main/java/be/vdab/groenetenen/web/WebConfiguratie.java
@@ -1,14 +1,19 @@
 package be.vdab.groenetenen.web;
 
-import java.util.Locale;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 //import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration		/*Je tikt @Configuration voor een class waarbinnen je met @Bean Spring beans maakt.*/
-class WebConfiguratie {
+class WebConfiguratie implements WebMvcConfigurer {
+/*
+ * Je moet elke interceptor registreren. Je doet dit in de huidige class.
+ * Deze class moet daartoe de interfafe WebMvcConfigurer implementeren.
+ */
 	
 	/*
 	 * Je gebruikt voor het eerst @Bean om een Spring bean te maken.
@@ -23,4 +28,17 @@ class WebConfiguratie {
 		return new SessionLocaleResolver();
 	}
 	
+	/*
+	 * Je doet de registratie van de interceptors zelf in de method addInterceptors, die gedeclareerd is in de interface WebMvcConfigurer.
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		/*
+		 * LocaleChangeInterceptor is een interceptor vh Spring framework.
+		 * Als de request een parameter locale bevat, 
+		 * roept de interceptor de method setLocal van de SessionLocaleResolver bean op en geeft die request parameter mee.
+		 * Je onthoudt zo de taal en het land dat de gebruiker gekozen heeft als HTTP session variabele.
+		 */
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
 }
